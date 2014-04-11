22e6f458ecc0b8237f98bb1c50b5f1f5864e87d5
Kevin DeMarco <kevin.demarco@gmail.com>
2014-04-11 16:15:26 -0400

    some tests

--- src/hello-lemon/main.cpp
+++ src/hello-lemon/main.cpp
@@ -9,9 +9,18 @@ int main()
      ListDigraph g;
      ListDigraph::Node u = g.addNode();
      ListDigraph::Node v = g.addNode();
+     ListDigraph::Node p = g.addNode();
      ListDigraph::Arc a = g.addArc(u, v);
      cout << "Hello World! This is LEMON library here." << endl;
      cout << "We have a directed graph with " << countNodes(g) << " nodes "
           << "and " << countArcs(g) << " arc." << endl;
+
+     cout << endl << endl;
+
+     int cnt = 0;
+     for (ListDigraph::NodeIt n(g); n != INVALID; ++n)
+          cnt++;
+     std::cout << "Number of nodes: " << cnt << std::endl;
+
      return 0;
 }
